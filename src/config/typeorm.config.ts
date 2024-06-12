e45bc294651d1aedf13cfc8f40340936a48d8a7d
Maksym Bocharov <Maksym Bocharov>
2024-06-12 22:27:00 +0300

    package-lock.json

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,12 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   port: 5432,
   username: 'postgres',
   password: 'root',
+  url: 'postgres://default:cKv39CHabUif@ep-broad-credit-a2k4y7e7.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require',
   database: 'gather',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true, // Set to false in production
+  autoLoadEntities: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
